510451757595cadb48036a7b6fd1bf5422126de8
y-ren16 <2367050142@qq.com>
2023-09-18 18:43:44 +0000

    demo create

---
+++ 1.sh
@@ -0,0 +1,13 @@
+cp ~/AcademiCodec/egs/HiFi-Codec-24k-320d/Paper_Data/GT/LibriTTS/0_7176_88083_000024_000007.wav ./GT/
+cp ~/AcademiCodec/egs/HiFi-Codec-24k-320d/Paper_Data/GEN/logs_bl_LibriTTS_1/LibriTTS/0_7176_88083_000024_000007.wav ./bl-1g1r/
+cp ~/AcademiCodec/egs/HiFi-Codec-24k-320d/Paper_Data/GEN/logs_bl_LibriTTS_1Group/LibriTTS/0_7176_88083_000024_000007.wav ./bl-1g2r/
+cp ~/AcademiCodec/egs/HiFi-Codec-24k-320d/Paper_Data/GEN/logs_bl_1g4r/LibriTTS/0_7176_88083_000024_000007.wav ./bl-1g4r/
+cp ~/AcademiCodec/egs/HiFi-Codec-24k-320d/Paper_Data/GEN/logs_convonly_Lib_1c_8g3k1s/LibriTTS/0_7176_88083_000024_000007.wav ./gl-1g1r/
+cp ~/AcademiCodec/egs/HiFi-Codec-24k-320d/Paper_Data/GEN/logs_convonly_Lib_1group_2c_8g3k1s/LibriTTS/0_7176_88083_000024_000007.wav ./gl-1g2r/
+cp ~/AcademiCodec/egs/HiFi-Codec-24k-320d/Paper_Data/GEN/logs_convonly_Lib_2group_4c_8g3k1s/LibriTTS/0_7176_88083_000024_000007.wav ./gl-1g4r/
+cp ~/AcademiCodec/egs/HiFi-Codec-24k-320d/Paper_Data/GEN/logs_convonly_Lib_1g1r_cos_from_head/LibriTTS/0_7176_88083_000024_000007.wav ./gl-1g1r-cos/
+cp ~/AcademiCodec/egs/HiFi-Codec-24k-320d/Paper_Data/GEN/logs_convonly_Lib_1g2r_cos_from_head/LibriTTS/0_7176_88083_000024_000007.wav ./gl-1g2r-cos/
+cp ~/AcademiCodec/egs/HiFi-Codec-24k-320d/Paper_Data/GEN/logs_convonly_Lib_1g4r_cos_from_head/LibriTTS/0_7176_88083_000024_000007.wav ./gl-1g4r-cos/
+cp ~/AcademiCodec/egs/HiFi-Codec-24k-320d/Paper_Data/GEN/logs_encodec_320d_output_25_1/LibriTTS/0_7176_88083_000024_000007.wav ./encodec-1/
+cp ~/AcademiCodec/egs/HiFi-Codec-24k-320d/Paper_Data/GEN/logs_encodec_320d_output_25_2/LibriTTS/0_7176_88083_000024_000007.wav ./encodec-2/
+cp ~/AcademiCodec/egs/HiFi-Codec-24k-320d/Paper_Data/GEN/logs_encodec_320d_output_25_4/LibriTTS/0_7176_88083_000024_000007.wav ./encodec-4/
---
+++ audios/valle/2.sh
@@ -0,0 +1,77 @@
+# cp ~/vall-e/egs/libritts/VALL-E-GEN-1/audios/valle_hifi_baseline_1codebooks_demos_test/121_121726_000025_000000-1811_from_121_121726_000029_000003-1806.wav bl-1g1r/
+# cp ~/vall-e/egs/libritts/VALL-E-GEN-1/audios/valle_hifi_baseline_nar_demos_test/121_121726_000025_000000-1811_from_121_121726_000029_000003-1806.wav bl-1g2r/                                                                      
+# cp ~/vall-e/egs/libritts/VALL-E-GEN-1/audios/valle_hifi_bl_1g4r_nar_demos_test/121_121726_000025_000000-1811_from_121_121726_000029_000003-1806.wav bl-1g4r/
+
+# cp ~/vall-e/egs/libritts/VALL-E-GEN-1/audios/valle_hifi_global_1codebooks_demos_test/121_121726_000025_000000-1811_from_121_121726_000029_000003-1806.wav gl-1g1r/
+# cp ~/vall-e/egs/libritts/VALL-E-GEN-1/audios/valle_hifi_gl_nar_demos_test/121_121726_000025_000000-1811_from_121_121726_000029_000003-1806.wav gl-1g2r/
+# cp ~/vall-e/egs/libritts/VALL-E-GEN-1/audios/valle_hifi_gl_1g4r_nar_demos_test/121_121726_000025_000000-1811_from_121_121726_000029_000003-1806.wav gl-1g4r/
+
+# cp ~/vall-e/egs/libritts/VALL-E-GEN-1/audios/valle_hifi_gl_1g1r_compare_cos_demos_test/121_121726_000025_000000-1811_from_121_121726_000029_000003-1806.wav gl-1g1r-cos/
+# cp ~/vall-e/egs/libritts/VALL-E-GEN-1/audios/valle_hifi_gl_1g2r_compare_cos_nar_demos_test/121_121726_000025_000000-1811_from_121_121726_000029_000003-1806.wav gl-1g2r-cos/
+# cp ~/vall-e/egs/libritts/VALL-E-GEN-1/audios/valle_hifi_gl_1g4r_compare_cos_nar_demos_test/121_121726_000025_000000-1811_from_121_121726_000029_000003-1806.wav gl-1g4r-cos/
+
+# cp ~/vall-e/egs/libritts/audios_libriTTS_test/121_121726_000025_000000-1811.wav ./target/
+
+# 121_121726_000029_000003-1806_from_121_127105_000038_000001-1830.wav
+
+# cp ~/vall-e/egs/libritts/VALL-E-GEN-1/audios/valle_hifi_baseline_1codebooks_demos_test/121_121726_000029_000003-1806_from_121_127105_000038_000001-1830.wav bl-1g1r/
+# cp ~/vall-e/egs/libritts/VALL-E-GEN-1/audios/valle_hifi_baseline_nar_demos_test/121_121726_000029_000003-1806_from_121_127105_000038_000001-1830.wav bl-1g2r/
+# cp ~/vall-e/egs/libritts/VALL-E-GEN-1/audios/valle_hifi_bl_1g4r_nar_demos_test/121_121726_000029_000003-1806_from_121_127105_000038_000001-1830.wav bl-1g4r/
+
+# cp ~/vall-e/egs/libritts/VALL-E-GEN-1/audios/valle_hifi_global_1codebooks_demos_test/121_121726_000029_000003-1806_from_121_127105_000038_000001-1830.wav gl-1g1r/
+# cp ~/vall-e/egs/libritts/VALL-E-GEN-1/audios/valle_hifi_gl_nar_demos_test/121_121726_000029_000003-1806_from_121_127105_000038_000001-1830.wav gl-1g2r/
+# cp ~/vall-e/egs/libritts/VALL-E-GEN-1/audios/valle_hifi_gl_1g4r_nar_demos_test/121_121726_000029_000003-1806_from_121_127105_000038_000001-1830.wav gl-1g4r/
+
+# cp ~/vall-e/egs/libritts/VALL-E-GEN-1/audios/valle_hifi_gl_1g1r_compare_cos_demos_test/121_121726_000029_000003-1806_from_121_127105_000038_000001-1830.wav gl-1g1r-cos/
+# cp ~/vall-e/egs/libritts/VALL-E-GEN-1/audios/valle_hifi_gl_1g2r_compare_cos_nar_demos_test/121_121726_000029_000003-1806_from_121_127105_000038_000001-1830.wav gl-1g2r-cos/
+# cp ~/vall-e/egs/libritts/VALL-E-GEN-1/audios/valle_hifi_gl_1g4r_compare_cos_nar_demos_test/121_121726_000029_000003-1806_from_121_127105_000038_000001-1830.wav gl-1g4r-cos/
+
+# cp ~/vall-e/egs/libritts/audios_libriTTS_test/121_121726_000029_000003-1806.wav ./target/
+
+# 260_123440_000023_000003-9_from_260_123440_000024_000002-6.wav
+
+# cp ~/vall-e/egs/libritts/VALL-E-GEN-1/audios/valle_hifi_baseline_1codebooks_demos_test/260_123440_000023_000003-9_from_260_123440_000024_000002-6.wav bl-1g1r/
+# cp ~/vall-e/egs/libritts/VALL-E-GEN-1/audios/valle_hifi_baseline_nar_demos_test/260_123440_000023_000003-9_from_260_123440_000024_000002-6.wav bl-1g2r/
+# cp ~/vall-e/egs/libritts/VALL-E-GEN-1/audios/valle_hifi_bl_1g4r_nar_demos_test/260_123440_000023_000003-9_from_260_123440_000024_000002-6.wav bl-1g4r/
+
+# cp ~/vall-e/egs/libritts/VALL-E-GEN-1/audios/valle_hifi_global_1codebooks_demos_test/260_123440_000023_000003-9_from_260_123440_000024_000002-6.wav gl-1g1r/
+# cp ~/vall-e/egs/libritts/VALL-E-GEN-1/audios/valle_hifi_gl_nar_demos_test/260_123440_000023_000003-9_from_260_123440_000024_000002-6.wav gl-1g2r/
+# cp ~/vall-e/egs/libritts/VALL-E-GEN-1/audios/valle_hifi_gl_1g4r_nar_demos_test/260_123440_000023_000003-9_from_260_123440_000024_000002-6.wav gl-1g4r/
+
+# cp ~/vall-e/egs/libritts/VALL-E-GEN-1/audios/valle_hifi_gl_1g1r_compare_cos_demos_test/260_123440_000023_000003-9_from_260_123440_000024_000002-6.wav gl-1g1r-cos/
+# cp ~/vall-e/egs/libritts/VALL-E-GEN-1/audios/valle_hifi_gl_1g2r_compare_cos_nar_demos_test/260_123440_000023_000003-9_from_260_123440_000024_000002-6.wav gl-1g2r-cos/
+# cp ~/vall-e/egs/libritts/VALL-E-GEN-1/audios/valle_hifi_gl_1g4r_compare_cos_nar_demos_test/260_123440_000023_000003-9_from_260_123440_000024_000002-6.wav gl-1g4r-cos/
+
+# cp ~/vall-e/egs/libritts/audios_libriTTS_test/260_123440_000023_000003-9.wav ./target/
+
+# 260_123440_000024_000001-11_from_260_123440_000023_000003-9
+
+cp ~/vall-e/egs/libritts/VALL-E-GEN-1/audios/valle_hifi_baseline_1codebooks_demos_test/260_123440_000024_000001-11_from_260_123440_000023_000003-9.wav bl-1g1r/
+cp ~/vall-e/egs/libritts/VALL-E-GEN-1/audios/valle_hifi_baseline_nar_demos_test/260_123440_000024_000001-11_from_260_123440_000023_000003-9.wav bl-1g2r/
+cp ~/vall-e/egs/libritts/VALL-E-GEN-1/audios/valle_hifi_bl_1g4r_nar_demos_test/260_123440_000024_000001-11_from_260_123440_000023_000003-9.wav bl-1g4r/
+
+cp ~/vall-e/egs/libritts/VALL-E-GEN-1/audios/valle_hifi_global_1codebooks_demos_test/260_123440_000024_000001-11_from_260_123440_000023_000003-9.wav gl-1g1r/
+cp ~/vall-e/egs/libritts/VALL-E-GEN-1/audios/valle_hifi_gl_nar_demos_test/260_123440_000024_000001-11_from_260_123440_000023_000003-9.wav gl-1g2r/
+cp ~/vall-e/egs/libritts/VALL-E-GEN-1/audios/valle_hifi_gl_1g4r_nar_demos_test/260_123440_000024_000001-11_from_260_123440_000023_000003-9.wav gl-1g4r/
+
+cp ~/vall-e/egs/libritts/VALL-E-GEN-1/audios/valle_hifi_gl_1g1r_compare_cos_demos_test/260_123440_000024_000001-11_from_260_123440_000023_000003-9.wav gl-1g1r-cos/
+cp ~/vall-e/egs/libritts/VALL-E-GEN-1/audios/valle_hifi_gl_1g2r_compare_cos_nar_demos_test/260_123440_000024_000001-11_from_260_123440_000023_000003-9.wav gl-1g2r-cos/
+cp ~/vall-e/egs/libritts/VALL-E-GEN-1/audios/valle_hifi_gl_1g4r_compare_cos_nar_demos_test/260_123440_000024_000001-11_from_260_123440_000023_000003-9.wav gl-1g4r-cos/
+
+cp ~/vall-e/egs/libritts/audios_libriTTS_test/260_123440_000024_000001-11.wav ./target/
+
+# 1580_141083_000041_000001-2155_from_1580_141083_000024_000000-2154
+
+cp ~/vall-e/egs/libritts/VALL-E-GEN-1/audios/valle_hifi_baseline_1codebooks_demos_test/1580_141083_000041_000001-2155_from_1580_141083_000024_000000-2154.wav bl-1g1r/
+cp ~/vall-e/egs/libritts/VALL-E-GEN-1/audios/valle_hifi_baseline_nar_demos_test/1580_141083_000041_000001-2155_from_1580_141083_000024_000000-2154.wav bl-1g2r/
+cp ~/vall-e/egs/libritts/VALL-E-GEN-1/audios/valle_hifi_bl_1g4r_nar_demos_test/1580_141083_000041_000001-2155_from_1580_141083_000024_000000-2154.wav bl-1g4r/
+
+cp ~/vall-e/egs/libritts/VALL-E-GEN-1/audios/valle_hifi_global_1codebooks_demos_test/1580_141083_000041_000001-2155_from_1580_141083_000024_000000-2154.wav gl-1g1r/
+cp ~/vall-e/egs/libritts/VALL-E-GEN-1/audios/valle_hifi_gl_nar_demos_test/1580_141083_000041_000001-2155_from_1580_141083_000024_000000-2154.wav gl-1g2r/
+cp ~/vall-e/egs/libritts/VALL-E-GEN-1/audios/valle_hifi_gl_1g4r_nar_demos_test/1580_141083_000041_000001-2155_from_1580_141083_000024_000000-2154.wav gl-1g4r/
+
+cp ~/vall-e/egs/libritts/VALL-E-GEN-1/audios/valle_hifi_gl_1g1r_compare_cos_demos_test/1580_141083_000041_000001-2155_from_1580_141083_000024_000000-2154.wav gl-1g1r-cos/
+cp ~/vall-e/egs/libritts/VALL-E-GEN-1/audios/valle_hifi_gl_1g2r_compare_cos_nar_demos_test/1580_141083_000041_000001-2155_from_1580_141083_000024_000000-2154.wav gl-1g2r-cos/
+cp ~/vall-e/egs/libritts/VALL-E-GEN-1/audios/valle_hifi_gl_1g4r_compare_cos_nar_demos_test/1580_141083_000041_000001-2155_from_1580_141083_000024_000000-2154.wav gl-1g4r-cos/
+
+cp ~/vall-e/egs/libritts/audios_libriTTS_test/1580_141083_000041_000001-2155.wav ./target/
